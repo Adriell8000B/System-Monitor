6e733d9b5e3f8b6f3c3bc9fcd3f6594e08cd6c40
Adriell8000B <adrielluiz73@gmail.com>
2025-10-22 10:10:01 -0300

    Fix: typo error

--- utils/cli-utils.c
+++ utils/cli-utils.c
@@ -2,15 +2,15 @@
 #include <stddef.h>
 #include <stdio.h>
 
-void line(char* linet_type, unsigned int line_size) {
-	if (linet_type == NULL || line_size == 0) {
-		printf("Parameter linet_type can't be NULL\nParameter size can't be 0");
+void line(char* line_type, unsigned int line_size) {
+	if (line_type == NULL || line_size == 0) {
+		printf("Parameter line_type can't be NULL\nParameter size can't be 0");
 		return;
 	}
 
 	for(size_t i = 0; i < line_size; i++) {
-		printf("%s", linet_type);
+		printf("%s", line_type);
 	}
 
-	printf("%s\n", linet_type);
+	printf("%s\n", line_type);
 }
