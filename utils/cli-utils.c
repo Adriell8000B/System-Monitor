8d2426a5327d8728094216e7e83542791e5118b6
Adriell8000B <adrielluiz73@gmail.com>
2025-10-22 10:08:50 -0300

    Remove useless functions

--- utils/cli-utils.c
+++ utils/cli-utils.c
@@ -2,28 +2,15 @@
 #include <stddef.h>
 #include <stdio.h>
 
-void line(char* type, unsigned int size) {
-	if (type == NULL || size == 0) {
-		printf("Parameter type can't be NULL\nParameter size can't be 0");
+void line(char* linet_type, unsigned int line_size) {
+	if (linet_type == NULL || line_size == 0) {
+		printf("Parameter linet_type can't be NULL\nParameter size can't be 0");
 		return;
 	}
 
-	for(size_t i = 0; i < size; i++) {
-		printf("%s", type);
+	for(size_t i = 0; i < line_size; i++) {
+		printf("%s", linet_type);
 	}
 
-	printf("%s\n", type);
-}
-
-void menu() {
-	char* options[4] = {
-		"[1] ",
-		"[2] hey",
-		"[3] hiy",
-		"[4] hoy"
-	};
-
-	for(size_t i = 0; i < 4; i++) {
-		printf("%s\n", options[i]);
-	}
+	printf("%s\n", linet_type);
 }
